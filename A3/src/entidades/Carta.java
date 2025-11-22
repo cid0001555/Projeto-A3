366d07aa8cff77221ebc7f800ae1598c20030108
cid <cidemailcorporativo@gmail.com>
2025-11-22 14:39:19 -0300

    Update Carta.java

--- A3/src/entidades/Carta.java
+++ A3/src/entidades/Carta.java
@@ -1,47 +1,26 @@
 package entidades;
 
 public class Carta extends Produto {
-	private Integer Quantidade;
-	private Integer QuantidadeTotal;
 
-	public Carta(String nome, double valor, String tipo, int QuantidadeTotal, int Quantidade) {
+	private String raridade;
+
+	public Carta(String nome, double valor, String tipo, String raridade) {
 		super(nome, valor, tipo);
-		this.Quantidade = Quantidade;
-		this.QuantidadeTotal = QuantidadeTotal;
+		this.raridade = raridade;
 	}
 
-	public static void Broche(Carta C) {
-		if (C.getQuantidadeTotal() == C.getQuantidade()) {
-			System.out.println("Coleção completa");
-		} else {
-			System.out.println("Coleção incompleta");
-		}
+	public String getRaridade() {
+		return raridade;
 	}
 
-	public Integer getQuantidade() {
-		return Quantidade;
-	}
-
-	public void setQuantidade(Integer quantidade) {
-		Quantidade = quantidade;
-	}
-
-	public Integer getQuantidadeTotal() {
-		return QuantidadeTotal;
-	}
-
-	public void setQuantidadeTotal(Integer quantidadeTotal) {
-		QuantidadeTotal = quantidadeTotal;
-	}
-
-	{
-		System.out.println("Coleção incompleta");
+	public void setRaridade(String raridade) {
+		this.raridade = raridade;
 	}
 
 	@Override
 	public void exibirInfo() {
 		super.exibirInfo();
-		System.out.println("Quantidade Total : " + QuantidadeTotal);
-		System.out.println("Quantidade : " + Quantidade);
+		System.out.println("Raridade:" + raridade);
 	}
+
 }
